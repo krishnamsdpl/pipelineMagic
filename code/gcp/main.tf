85ffdc5de44d0939f324a94cd7ed54ef79a2e076
krishnamsdpl <krishnagogula2@gmail.com>
2023-11-20 09:48:34 +0530

    added new line in pipeline

---
+++ code/gcp/main.tf
@@ -0,0 +1,27 @@
+# Specify the GCP provider configuration
+provider "google" {
+  project     = "firm-mariner-405508"  # Remove the curly braces from the project ID
+  credentials = file("C:/Users/LENOVO/Documents/projects/pipelineMagic/code/gcp/key/firm-mariner-405508-c0022e7d63fa.json")
+  region      = "us-central1"
+  
+}
+
+# Create a GCP instance
+resource "google_compute_instance" "my_instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Ephemeral IP
+    }
+  }
+}
