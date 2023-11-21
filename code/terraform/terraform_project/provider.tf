85403fce49466c2d38508f799413ccf9d3c45613
Krishna.Gogula <Krishna.Gogula@royalmail.onmicrosoft.com>
2023-11-21 12:17:06 +0530

    updated pipeline

--- code/terraform/terraform_project/provider.tf
+++ code/terraform/terraform_project/provider.tf
@@ -4,13 +4,4 @@ provider "azurerm" {
   client_id       = "your_client_id"
   client_secret   = "your_client_secret"
   tenant_id       = "your_tenant_id"
-  // ... other parameters
 }
-
-# Include the variables.tf file
-terraform {
-  required_version = ">= 0.12.0"
-  source = "./variables.tf"
-}
-
-# Other configurations go here...
